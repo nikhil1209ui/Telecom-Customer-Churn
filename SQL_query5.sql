88f16c58b89bc462bfaa2fbb0d2791fe7b0f31e5
Nikhil kushwaha <135628014+nikhil1209ui@users.noreply.github.com>
2025-01-19 19:19:46 +0530

    Delete SQL_query5.sql

--- SQL_query5.sql
+++
@@ -1,9 +0,0 @@
---Create View for Power BI
-
-Create View vw_ChurnData as
-    select * from prod_Churn where Customer_Status In ('Churned', 'Stayed')
-
-Create View vw_JoinData as
-    select * from prod_Churn where Customer_Status = 'Joined'
-
-select * from vw_ChurnData
